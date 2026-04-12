6a815ec5666504de9a93b00ef4804ba38ff4d395
NitheeshNik <146535374+NitheeshNik@users.noreply.github.com>
2026-04-12 12:18:40 +0530

    learn arithmetic operators

---
+++ 01.basics/arithmetic_operators.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+
+    // arithmetic operators = + - * / % ++ --
+
+    int x = 2;
+    int y = 3;
+    int z = 0;
+
+    // z = x + y;
+    // z = x - y;
+    // z = x * y;
+    // z = x / y;
+    // z=x % 2;
+
+    // x+=3;
+    // x-=3;
+    // x*=2;
+    // x/=2;
+    printf("%d\n", z);
+    return 0;
+}
