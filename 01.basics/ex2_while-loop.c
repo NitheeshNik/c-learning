b74fc3db79c19be7f0fc8cee3990976fb0f9e4f3
NitheeshNik <146535374+NitheeshNik@users.noreply.github.com>
2026-05-10 18:34:29 +0530

    while loop

---
+++ 01.basics/ex2_while-loop.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main()
+{
+
+    bool isRunning = true;
+    char response = '\0';
+
+    while (isRunning)
+    {
+        printf("You are playing a game\n");
+        printf("Would you like to continue? (Y = yes, N = no): ");
+        scanf(" %c", &response);
+
+        if (response != 'Y' && response != 'y')
+        {
+            isRunning = false;
+        }
+    }
+
+    printf("You exit the game");
+
+    return 0;
+}
--- 01.basics/while-loop.c
+++ 01.basics/while-loop.c
@@ -5,12 +5,20 @@ int main()
     // while loop = Continue some code WHILE the condition remains true
     //              Condition must be true for us to enter while loop
     int number = 0;
+    int num1 = 1; // do while
 
     while (number <= 0)
     {
         printf("Enter a number greater than 0: ");
         scanf("%d", &number);
     }
+
+    // do while loop
+    do
+    {
+        printf("Enter a number greater than 1: ");
+        scanf("%d", &num1);
+    } while (num1 <= 1);
 
     return 0;
 }
