3fea57898463e0174d5f42994fbc70f3bc29c457
NitheeshNik <146535374+NitheeshNik@users.noreply.github.com>
2026-05-14 23:11:47 +0530

    Pseudo-random numbers to i learn to generate random numbers in c

--- 01.basics/random_numbers.c
+++ 01.basics/random_numbers.c
@@ -11,5 +11,15 @@ int main()
     //                 advanced: Mersenne Twister or /dev/random
     srand(time(NULL));
 
+    int min = 100;
+    int max = 50;
+    // printf("%d\n", RAND_MAX); // rand() also use this.
+
+    int randomNum1 = (rand() % (max - min + 1)) + min;
+    int randomNum2 = (rand() % (max - min + 1)) + min;
+    int randomNum3 = (rand() % (max - min + 1)) + min;
+
+    printf("%d %d %d", randomNum1, randomNum2, randomNum3);
+
     return 0;
 }
