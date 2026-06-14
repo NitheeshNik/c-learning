15dc41bf0216ae5d80bbb37f45537f943f7d7aae
NitheeshNik <146535374+NitheeshNik@users.noreply.github.com>
2026-06-14 11:38:38 +0530

    modified

--- 01.basics/number-gussing-game.c
+++ 01.basics/number-gussing-game.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <time.h>
 int main()
 {
-    printf("NUMBER GUSSING GAME\n");
+    srand(time(NULL));
+
+    printf("%d\n", rand());
+    printf("%d\n", rand());
+    printf("%d\n", rand());
     return 0;
 }
