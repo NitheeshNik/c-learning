e992b879bf492abdf78f609bad72f97f32ea394e
NitheeshNik <146535374+NitheeshNik@users.noreply.github.com>
2026-06-19 01:30:45 +0530

    some thing modified

--- 01.basics/rock_paper_scissors.c
+++ 01.basics/rock_paper_scissors.c
@@ -9,13 +9,17 @@ int main()
     int computerChioce, rock = 0, paper = 1, scissors = 2, attempt = 0, userGuess;
 
     computerChioce = rand() % 3;
+
     printf("%d", computerChioce);
+
     printf("Enter your guess rock=0,paper=1,scissors=3: ");
+
     scanf("%d", &userGuess);
 
-    if (computerChioce == userGuess)
+    if (computerChioce > userGuess)
     {
         printf("you win");
     }
-    return 0;
+    else if ()
+        return 0;
 }
