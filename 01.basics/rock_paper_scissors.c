c9243a310cc75128b5fe8f23f6dbf45f77c36cb4
NitheeshNik <146535374+NitheeshNik@users.noreply.github.com>
2026-06-19 01:30:11 +0530

    still progress

---
+++ 01.basics/rock_paper_scissors.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    // ROCK PAPER SCISSORS
+    srand(time(NULL));
+    int computerChioce, rock = 0, paper = 1, scissors = 2, attempt = 0, userGuess;
+
+    computerChioce = rand() % 3;
+    printf("%d", computerChioce);
+    printf("Enter your guess rock=0,paper=1,scissors=3: ");
+    scanf("%d", &userGuess);
+
+    if (computerChioce == userGuess)
+    {
+        printf("you win");
+    }
+    return 0;
+}
