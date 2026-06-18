43abaad3b9e43d552148d22f92d3acc758f6942d
NitheeshNik <146535374+NitheeshNik@users.noreply.github.com>
2026-06-18 21:16:41 +0530

    almost complete

--- 01.basics/number-gussing-game.c
+++ 01.basics/number-gussing-game.c
@@ -4,10 +4,28 @@
 
 int main()
 {
-    printf("NUMBER GUSSING GAME\n");
+    int x = 0;
+    int guess = 0;
 
-    int random = rand();
-    printf("%d\n", random);
+    printf("NUMBER GUSSING GAME\n");
+    srand(time(NULL));
+
+    x = rand() % 10;
+    printf("%d\n", x);
+    printf("Enter your number: \n");
+    scanf("%d", &guess);
+    if (x > guess)
+    {
+        printf("Higer");
+    }
+    else if (x < guess)
+    {
+        printf("Lower");
+    }
+    else
+    {
+        printf("You win");
+    }
 
     return 0;
 }
