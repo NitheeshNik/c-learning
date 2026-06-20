57e388d9adeec19a6346e73be740b76b1e071639
NitheeshNik <146535374+NitheeshNik@users.noreply.github.com>
2026-06-20 14:48:27 +0530

    New project rock paper scissors.c

--- 01.basics/rock_paper_scissors.c
+++ 01.basics/rock_paper_scissors.c
@@ -2,24 +2,14 @@
 #include <stdlib.h>
 #include <time.h>
 
+int getComputerChoice();
+int getUserChoice();
+void checkWinner(int userChoice, int computerChoice);
+
 int main()
 {
-    // ROCK PAPER SCISSORS
-    srand(time(NULL));
-    int computerChioce, rock = 0, paper = 1, scissors = 2, attempt = 0, userGuess;
 
-    computerChioce = rand() % 3;
+    //  ROCK PAPER SCISSORS
 
-    printf("%d", computerChioce);
-
-    printf("Enter your guess rock=0,paper=1,scissors=3: ");
-
-    scanf("%d", &userGuess);
-
-    if (computerChioce > userGuess)
-    {
-        printf("you win");
-    }
-    else if ()
-        return 0;
+    return 0;
 }
