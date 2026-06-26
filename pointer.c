af7bd25e7de75f8c728ddf93aaa4b5d7c9018351
NitheeshNik <146535374+NitheeshNik@users.noreply.github.com>
2026-06-26 16:21:59 +0530

    new topic start

---
+++ pointer.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+
+    //   pointer = A variable that stores the memory address of another variable.
+    //             Benefit: They help avoid wasting memory by allowing you to pass
+    //             the address of a large data structure instead of copying the entire data.
+
+    return 0;
+}
