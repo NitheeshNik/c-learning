5622d0e2cfc055aca276b63d8fdae93191a8430a
NitheeshNik <146535374+NitheeshNik@users.noreply.github.com>
2026-06-26 16:50:36 +0530

    i learned pointers

--- pointer.c
+++ pointer.c
@@ -1,11 +1,25 @@
 #include <stdio.h>
 
+void birthday(int *age);
+
 int main()
 {
-
+    // pointer format specifiar = %p
+    // * = dereference operator
     //   pointer = A variable that stores the memory address of another variable.
     //             Benefit: They help avoid wasting memory by allowing you to pass
     //             the address of a large data structure instead of copying the entire data.
+    int age = 25;
+
+    birthday(&age);
+    printf("You are %d years Old\n", age);
 
     return 0;
+}
+
+void birthday(int *age)
+{
+
+    // pass by reference
+    (*age)++;
 }
