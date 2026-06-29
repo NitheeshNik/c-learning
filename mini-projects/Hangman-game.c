d4e61e4b17b4ea347b0dbca96fbfe61d500b77a4
NitheeshNik <146535374+NitheeshNik@users.noreply.github.com>
2026-06-29 16:54:32 +0530

    practice project

--- mini-projects/Hangman-game.c
+++ mini-projects/Hangman-game.c
@@ -3,7 +3,10 @@
 int main()
 {
 
-    printf("******HANGMAN GAME******");
+    printf("******HANGMAN GAME******\n");
+    int x = 10;
+    int *ptr = &x; // The '&' operator gets the physical RAM address of x
+    printf("The value of x is %d, and it lives at memory address %p", x, ptr);
 
     return 0;
 }
