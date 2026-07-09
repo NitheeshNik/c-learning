66ada6d48c30378cb0f8c90f4436f3fda3625872
NitheeshNik <146535374+NitheeshNik@users.noreply.github.com>
2026-07-09 23:23:03 +0530

    name change

---
+++ practice-program/Hangman-game.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <time.h>
+
+int main()
+{
+    char words[10][50] = {"Camel", "Cat", "Cheetah", "Chimpanzee", "Cobra",
+                          "Coyote", "Crab", "Crane", "Crocodile", "Crow"};
+
+        return 0;
+}
