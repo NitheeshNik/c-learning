7f7063ead541a8e4b9d47ad180ef00b46ef5c98c
NitheeshNik <146535374+NitheeshNik@users.noreply.github.com>
2026-07-22 22:35:27 +0530

    hangmap game start building

--- practice-program/Hangman-game.c
+++ practice-program/Hangman-game.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-#include <time.h>
 
 int main()
 {
-    char words[10][50] = {"Camel", "Cat", "Cheetah", "Chimpanzee", "Cobra",
-                          "Coyote", "Crab", "Crane", "Crocodile", "Crow"};
-
-        return 0;
+    puts("Print welcome message.");
+    printf("Print welcome message.");
+    char grade = 'a';
+    putchar(grade);
+    putchar('\n');
+    return 0;
 }
