a70068add1dee3a693bae8e1ecfe379777c0feb5
NitheeshNik <146535374+NitheeshNik@users.noreply.github.com>
2026-08-23 13:19:38 +0530

    no changes

--- practice-program/Hangman-game.c
+++ practice-program/Hangman-game.c
@@ -4,7 +4,10 @@
 
 int main()
 {
+    char name;
     char arr[4][20] = {"cat", "dog", "cow", "pig"};
     printf("%s", arr[0]);
+    printf("enter user name: ");
+    scanf("%s", &name);
     return 0;
 }
